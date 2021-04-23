a5db42e1b2df604416c19fc0bb50fb04b55fa648
Bruno Silva Santana <ibrunoss@outlook.com>
2021-04-23 19:36:40 -0300

    Fix UserController file name and import on routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
-import { UsersController } from "./controllers/UsersCrontroller";
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
